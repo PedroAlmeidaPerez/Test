34de972c5971351a093e0cd6b0ef2883f6588ec0
pedro <pedro.almeida.perez@gmail.com>
2020-02-17 16:44:46 +0100

    User

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class App {
+    public static void main(String[] args){
+
+        //ADD USER AND SHOW
+        UserService userService = new UserService();
+        User user1 = new User ("Marcin", 35);
+        User user2 = new User ("pedro", 45);
+        userService.AddUserList(user1);
+        userService.AddUserList(user2);
+
+
+        userService.ShowList();
+
+        //DELETE USER AND SHOW
+        userService.DeleteUser("Marcin");
+
+        userService.ShowList();
+
+
+    }
 }
+
